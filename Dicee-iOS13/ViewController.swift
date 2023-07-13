4212280496702fc7ba1d285af316e5e4c1e97e62
Mehmet Güven <guvennmehmet37@gmail.com>
2023-07-13 15:14:50 +0300

    randomize the dice images

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [UIImage(named: "DiceOne"),
+                        UIImage(named: "DiceTwo"),
+                        UIImage(named: "DiceThree"),
+                        UIImage(named: "DiceFour"),
+                        UIImage(named: "DiceFive"),
+                        UIImage(named: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
+    
 }
 
